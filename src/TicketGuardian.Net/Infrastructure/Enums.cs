1f61af45e73288019d961a39b8094daf2577cdeb
Paul Oliveira <poliveira@matrixwebs.com>
2018-06-13 09:07:37 -0700

    Added PriceType and ClaimStatus enums Added initial Urls

---
+++ src/TicketGuardian.Net/Infrastructure/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace TicketGuardian.Net.Infrastructure
+{
+    public enum PriceTypes
+    {
+        [Description("Fixed")]
+        @fixed,
+        [Description("Percent")]
+        percent
+    }
+
+    public enum ClaimStatuses
+    {
+        [Description("Initiated")]
+        initiated,
+        [Description("Paid")]
+        paid,
+        [Description("Docs Required")]
+        docsrequired,
+        [Description("Denied")]
+        denied
+    }
+}
---
+++ src/TicketGuardian.Net/Infrastructure/Urls.cs
@@ -0,0 +1,26 @@
+﻿namespace TicketGuardian.Net.Infrastructure
+{
+    internal static class Urls
+    {
+        static string LiveApiUrl
+        {
+            get
+            {
+                return "https://connect.ticketguardian.net/api/v1";
+            }
+        }
+
+        static string SandboxApiUrl
+        {
+            get
+            {
+                return "https://connect-test.ticketguardian.net/api/v1";
+            }
+        }
+
+        public static string ApiUrl(bool sandboxEnabled)
+        {
+            return sandboxEnabled ? SandboxApiUrl : LiveApiUrl;
+        }
+    }
+}
