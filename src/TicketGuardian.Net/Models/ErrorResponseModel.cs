083cba191fd0ed75b43c470ee140f412b1096f50
Paul Oliveira <poliveira@matrixwebs.com>
2018-06-13 09:04:05 -0700

    Initial setup and commit

---
+++ src/TicketGuardian.Net/Models/ErrorResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace TicketGuardian.Net.Models
+{
+    public class ErrorResponseModel
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonIgnore]
+        public string Url { get; set; }
+    }
+}
---
+++ src/TicketGuardian.Net/TicketGuardianException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using TicketGuardian.Net.Models;
+
+namespace TicketGuardian.Net
+{
+    public class TicketGuardianException : Exception
+    {
+        #region Properties
+        public HttpStatusCode HttpStatusCode { get; private set; }
+        public ErrorResponseModel ErrorResponse { get; private set; }
+        #endregion
+
+        #region ctors
+        public TicketGuardianException()
+        {
+
+        }
+
+        public TicketGuardianException(HttpStatusCode statusCode, ErrorResponseModel error, string message) : base(message)
+        {
+            HttpStatusCode = statusCode;
+            ErrorResponse = error;
+        }
+        #endregion
+    }
+}
